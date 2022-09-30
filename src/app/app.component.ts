31a6040b8ad28e561e6d03d656894115ff791994
Majid <majid.jalilvand2000@gmail.com>
2022-09-30 09:13:13 +0200

    Added initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bahar-shopping';
+
+  advertismentimages: any[] = ['../assets/images.jpg', '../assets/12.jpg', '../assets/10.jpg',
+    '../assets/iphone_13_pro_max_.webp', '../assets/note-10.jpg'];
+  selectedAdvertismentIndex: number = 0;
+  selectedAdvertismentImageUrl: string = '../assets/images.jpg';
+
+  nextImage() {
+    this.selectedAdvertismentIndex++
+    if (this.selectedAdvertismentIndex === this.advertismentimages.length) {
+      this.selectedAdvertismentIndex = 0;
+    }
+    this.selectedAdvertismentImageUrl = this.advertismentimages.find((val, i) => i === this.selectedAdvertismentIndex)
+
+  }
+
+  backImage() {
+    this.selectedAdvertismentIndex--
+    if (this.selectedAdvertismentIndex === -1) {
+      this.selectedAdvertismentIndex = this.advertismentimages.length - 1;
+    }
+    this.selectedAdvertismentImageUrl = this.advertismentimages.find((val, i) => i === this.selectedAdvertismentIndex)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
