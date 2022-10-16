d9b3dcda0401e3601c18ced85a6c46e17864a7e6
Majid <majid.jalilvand2000@gmail.com>
2022-10-16 14:01:35 +0200

    Changed bestselling flex

--- src/app/bestselling/bestselling.component.ts
+++ src/app/bestselling/bestselling.component.ts
@@ -14,20 +14,10 @@ export class BestsellingComponent implements OnInit {
     { imageUrl: '../../../../assets/khame.jpg', name: 'Body', availableStock: 17, price: '50.56' },
     { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' },
     { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' },
-    { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' }]
-  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  // dataSource: PeriodicElement[] = [
-  //   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  //   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  //   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  //   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  //   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  //   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  //   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  //   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  //   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  //   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  // ];;
+    { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' },
+    { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' },
+    { imageUrl: '../../../../assets/molfix.jpg', name: 'TV', availableStock: 857, price: '2035.56' },]
+
   constructor() { }
 
   ngOnInit(): void {
